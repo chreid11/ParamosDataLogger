918006a770f90eeb0cd38089cfc24262a75edf43
chreid11 <120068646+chreid11@users.noreply.github.com>
2023-01-31 23:04:42 -0500

    pin voltage test

---
+++ tests/gpio_test/gpio_test.ino
@@ -0,0 +1,44 @@
+/*test gpio voltages
+*/
+const int warmUpTime = 5000;
+
+void setup () {
+
+    delay(warmUpTime);
+    //measure here
+    //----
+
+    //----
+    //set output pins
+    pinMode(2, OUTPUT);
+    pinMode(0, OUTPUT);
+    pinMode(13, OUTPUT);
+    pinMode(12, OUTPUT);
+    pinMode(14, OUTPUT);
+    //----
+
+
+    delay(warmUpTime);
+    //measure here
+
+}
+
+
+void loop () {
+  
+    digitalWrite(2, LOW);
+    digitalWrite(0, LOW);
+    digitalWrite(13, LOW);
+    digitalWrite(12, LOW);
+    digitalWrite(14, LOW);
+    delay(warmUpTime);
+    //measure here
+
+    digitalWrite(2, HIGH);
+    digitalWrite(0, HIGH);
+    digitalWrite(13, HIGH);
+    digitalWrite(12, HIGH);
+    digitalWrite(14, HIGH);
+    delay(warmUpTime);
+    //measure here
+}
